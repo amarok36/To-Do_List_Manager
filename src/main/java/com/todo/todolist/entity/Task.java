444b5f40b929bce843fd6d827c393ad9b32b7007
amarok36 <bobrowww@mail.ru>
2025-08-23 12:21:49 +0500

    The controller has been expanded and switched to work with R2DBC

--- src/main/java/com/todo/todolist/entity/Task.java
+++ src/main/java/com/todo/todolist/entity/Task.java
@@ -1,18 +1,14 @@
 package com.todo.todolist.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 import lombok.Data;
-
 import java.time.LocalDate;
 
-@Entity
 @Data
+@Table("task")
 public class Task {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
